cc3aee5f624f2aa75f54d31e875b0ee7a72230ae
Jashjeet Singh <jashjeet99@gmail.com>
2026-05-22 05:31:32 +0530

    Insertion and Display of Singly Linked-List

---
+++ SLinkedList.c
@@ -0,0 +1,60 @@
+// Insertion and Display of Singly Linked-List
+
+# include <stdio.h>
+# include <stdlib.h>
+
+struct singleList {
+    int info;
+    struct singleList* next;
+};
+typedef struct singleList node;
+
+node* createList(node* start, int num) {
+    node* newNode;
+    newNode = (node*) malloc(sizeof(node));
+    newNode->info = num;
+    newNode->next = NULL;
+    if(NULL == start) {
+        start = newNode;
+    }
+    else {
+        node* lastNode;
+        lastNode = start;
+        while(lastNode->next != NULL) {
+            lastNode = lastNode->next;
+        }
+        lastNode->next = newNode;
+    }
+    return start;
+}
+
+void displayList(node* start) {
+    if(start == NULL) {
+        printf("List is empty");
+        return;
+    }
+    printf("List elements are");
+    while(start != NULL) {
+        printf("%d\t", start->info);
+        start = start->next;
+    }
+    return;
+}
+
+int main(void) {
+    node* createList(node*, int);
+    void displayList(node*);
+    node* start;
+    int num;
+    int another = 1;
+    start = NULL;
+    while(another == 1) {
+        printf("Enter the number to insert");
+        scanf("%d", &num);
+        start = createList(start,num);
+        printf("YES/NO 1/0");
+        scanf("%d", &another);
+    }
+    displayList(start);
+    return 0;
+}
